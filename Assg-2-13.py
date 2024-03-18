884f68b42438302cd076cf34238c47eaff628bcc
bimotrig <bimotrigoutomo06@gmail.com>
2024-03-18 09:49:32 +0900

    Assignment 2 - iSEED Programming

---
+++ Assg-2-13.py
@@ -0,0 +1,3 @@
+print("int('1002',2) error because the 1002 is not a number with base 2, so cannot converted into decimal")
+print("int('1008',8) error because the 1008 is not a number with base 8, so cannot converted into decimal")
+print("int('AAFG',16) error because the AAFG is not a number with base 16, so cannot converted into decimal")
---
+++ Assg-2-15.py
@@ -0,0 +1,8 @@
+num = 12345678
+hex_num = hex(num)
+oct_num = oct(num)
+bin_num = bin(num) 
+print ("decimal ==>", num)
+print ("hex_num ==>", hex_num)
+print ("hex_num ==>", oct_num)
+print ("hex_num ==>", bin_num)
